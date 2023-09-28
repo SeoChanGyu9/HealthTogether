522dabf6844a814dfa881541122ec6a1db18cc43
KHJ12346 <bestkang3@gmail.com>
2023-09-28 17:51:56 +0900

    필요클래스 + 추가작업

---
+++ app/src/main/java/com/example/firebase_test/MessageAdapter.java
@@ -0,0 +1,63 @@
+package com.example.firebase_test;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHolder> {
+
+    List<Message> messageList;
+    public MessageAdapter(List<Message> messageList) {
+        this.messageList = messageList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.gpt_item,null);
+        MyViewHolder myViewHolder = new MyViewHolder(chatView);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Message message = messageList.get(position);
+        if(message.getSentBy().equals(Message.SENT_BY_ME)){  //본인(나)가 보낸 메세지일 때
+            holder.leftChatView.setVisibility(View.GONE);  // 왼쪽 챗뷰숨김
+            holder.rightChatView.setVisibility(View.VISIBLE);  // 오른쪽챗뷰 보임
+            holder.rightTextView.setText(message.getMessage());
+
+        }else{                          //봇이 보낸 메세지일 시 위와 반대
+            holder.rightChatView.setVisibility(View.GONE);
+            holder.leftChatView.setVisibility(View.VISIBLE);
+            holder.leftTextView.setText(message.getMessage());
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout leftChatView,rightChatView;
+        TextView leftTextView,rightTextView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            leftChatView = itemView.findViewById(R.id.left_chat_view);
+            rightChatView = itemView.findViewById(R.id.right_chat_view);
+            leftTextView = itemView.findViewById(R.id.left_chat_text_view);
+            rightTextView = itemView.findViewById(R.id.right_chat_text_view);
+        }
+        }
+    }
--- app/src/main/java/com/example/firebase_test/gpt_mes_test.java
+++ app/src/main/java/com/example/firebase_test/gpt_mes_test.java
@@ -2,33 +2,67 @@ package com.example.firebase_test;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class gpt_mes_test extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView welcomeTextView;
     EditText messageEditText;
     ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gpt_mes);
+        messageList = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recycler_view);
         welcomeTextView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findViewById(R.id.send_btn);
 
+        //뷰
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
         sendButton.setOnClickListener((v)->{
             String question = messageEditText.getText().toString().trim();
-            Toast.makeText(this,question,Toast.LENGTH_LONG).show();
+            addToChat(question,Message.SENT_BY_ME);
+            messageEditText.setText(""); // 메세지 보내고 나면 메세지 입력창 초기화
+            welcomeTextView.setVisibility(View.GONE);
         });
     }
+
+    void addToChat(String message,String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+
+            }
+        });
+    }
+
+
+
+
+
 }
